86c2e4dfc4853daa6d8eccfc61cb348e8b4cfad6
aditya136 <adityabanerjee102@gmail.com>
2022-06-15 21:47:34 +0600

    new codes in python

--- t.py
+++ t.py
@@ -1,1 +1,5 @@
-# Just for test
+print("Hello sir,I try to make a program like:if you whant to sent some massege so this program can help you")
+
+answer = input("what is yout plat from:")
+print(f"ok")
+
